8bf8280e7e70c97cae6709ef36f777d3837c191d
Janhvi Patil <janhvipatil716@gmail.com>
2024-10-04 14:21:33 +0530

    feat: added vercel web analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from '@vercel/analytics/react';
 import "./globals.css";
 import "cal-sans";
 
@@ -36,6 +37,7 @@ export default function RootLayout({
       <body className="antialiased">
         {children}
         <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
